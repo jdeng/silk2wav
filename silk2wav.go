d7f567fadb374f3d6d8ead8c643334324272e370
Jack <jackdeng@gmail.com>
2018-03-18 18:24:30 -0700

    as library

--- silk2wav.go
+++ silk2wav.go
@@ -1,12 +1,10 @@
-package main
+package silk2wav
 
 import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
 	"unsafe"
 )
 
@@ -101,24 +99,4 @@ func Convert(buf []byte) ([]byte, error) {
 	return b.Bytes(), nil
 }
 
-func main() {
-	if len(os.Args) != 3 {
-		fmt.Printf("Usage: %s inputfile outputfile\n", os.Args[0])
-		return
-	}
 
-	data, err := ioutil.ReadFile(os.Args[1])
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	out, err := Convert(data)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	ioutil.WriteFile(os.Args[2], out, 0755)
-	log.Printf("Converted %s to %s\n", os.Args[1], os.Args[2])
-}
